f89500e7119521ec0f6c1d197a43786754515e4e
jayarghargh <jayarghargh@gmail.com>
2021-03-24 08:14:41 +1000

    redo model to allow for specific information over generalised json input

--- models/workout.js
+++ models/workout.js
@@ -1,17 +1,29 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now
+const WorkoutSchema = new Schema({
+    day: {type: Date, default: Date.now},
+    exercises: [{
+        type: { type: String, trim: true, required: "Please enter an exercise type."},
+        name: { type: String, trim: true, required: "Please enter a name."},
+        weight: { type: Number},
+        sets: { type: Number},
+        reps: { type: Number},
+        duration: { type: Number},
+        distance: { type: Number}
+    }]
   },
-  exercises: {
-    type: JSON,
-  }
+  { toJSON: { virtuals: true} }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  // const reducer = (sum, exercise) => sum + exercise.duration;
+  // return this.exercises.reduce(reducer());
+  return this.exercises.reduce(( sum, exercise ) => {
+    return sum + exercise.duration;
+  }, 0);
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
